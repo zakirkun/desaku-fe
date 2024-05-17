a1bbe28052e35a83ab8636d17bc2563891223263
ichsanputr <ngekode24@gmail.com>
2024-05-17 10:31:17 +0700

    header

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
+import { onClickOutside } from '@vueuse/core'
+
 
 const headerActive = ref(false)
 const windowWidth = ref(null)
 const navMobile = ref(false)
+const target = ref(null)
 
 const headerData = reactive({
     no_telp: null,
@@ -31,6 +34,8 @@ async function loadHeaderTop() {
     headerData.no_telp = data.no_telp
     headerData.email = data.email
 }
+
+onClickOutside(target, event => navMobile.value = false)
 
 definePageMeta({
     layout: false
@@ -103,34 +108,37 @@ export default {
 }
 </script>
 <template>
-    <div v-if="navMobile" class="bg-white h-screen fixed w-3/4 right-0 animatenav" style="z-index: 9999;">
-        <div class="block h-screen px-3 py-4">
-            <div class="flex justify-between cursor-pointer border-b border-slate-200 pb-4" @click="$router.push('/')">
-                <div class="flex">
-                    <img width="40"
-                        src="https://kertamulya-padalarang.desa.id/assets/files/data/website-desa-kertamulya-3217082001/images/logo_header.png"
-                        alt="">
-                    <div class="ml-3 block font-semibold">
-                        <div>
-                            <span>Desa Kertamulya</span>
-                        </div>
-                        <div class="text-sm">
-                            <span>Kabupaten Kulon Progo</span>
+    <Transition>
+        <div ref="target" v-if="navMobile" class="bg-white h-screen fixed w-3/4 right-0" style="z-index: 9999;">
+            <div class="block h-screen px-3 py-4">
+                <div class="flex justify-between cursor-pointer border-b border-slate-200 pb-4"
+                    @click="$router.push('/')">
+                    <div class="flex-none flex">
+                        <img width="40"
+                            src="https://kertamulya-padalarang.desa.id/assets/files/data/website-desa-kertamulya-3217082001/images/logo_header.png"
+                            alt="">
+                        <div class="ml-3 block font-semibold">
+                            <div>
+                                <span>Desa Kertamulya</span>
+                            </div>
+                            <div class="text-sm">
+                                <span>Kabupaten Kulon Progo</span>
+                            </div>
                         </div>
                     </div>
+                    <div class="flex items-center" @click="navMobile = false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 28 28">
+                            <path fill="black"
+                                d="M20.48 3.512a11.966 11.966 0 0 0-8.486-3.514C5.366-.002-.007 5.371-.007 11.999c0 3.314 1.344 6.315 3.516 8.487A11.966 11.966 0 0 0 11.995 24c6.628 0 12.001-5.373 12.001-12.001c0-3.314-1.344-6.315-3.516-8.487m-1.542 15.427a9.789 9.789 0 0 1-6.943 2.876c-5.423 0-9.819-4.396-9.819-9.819a9.789 9.789 0 0 1 2.876-6.943a9.786 9.786 0 0 1 6.942-2.876c5.422 0 9.818 4.396 9.818 9.818a9.785 9.785 0 0 1-2.876 6.942z" />
+                            <path fill="black"
+                                d="m13.537 12l3.855-3.855a1.091 1.091 0 0 0-1.542-1.541l.001-.001l-3.855 3.855l-3.855-3.855A1.091 1.091 0 0 0 6.6 8.145l-.001-.001l3.855 3.855l-3.855 3.855a1.091 1.091 0 1 0 1.541 1.542l.001-.001l3.855-3.855l3.855 3.855a1.091 1.091 0 1 0 1.542-1.541l-.001-.001z" />
+                        </svg>
+                    </div>
                 </div>
-                <div class="flex items-center" @click="navMobile = false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 28 28">
-                        <path fill="black"
-                            d="M20.48 3.512a11.966 11.966 0 0 0-8.486-3.514C5.366-.002-.007 5.371-.007 11.999c0 3.314 1.344 6.315 3.516 8.487A11.966 11.966 0 0 0 11.995 24c6.628 0 12.001-5.373 12.001-12.001c0-3.314-1.344-6.315-3.516-8.487m-1.542 15.427a9.789 9.789 0 0 1-6.943 2.876c-5.423 0-9.819-4.396-9.819-9.819a9.789 9.789 0 0 1 2.876-6.943a9.786 9.786 0 0 1 6.942-2.876c5.422 0 9.818 4.396 9.818 9.818a9.785 9.785 0 0 1-2.876 6.942z" />
-                        <path fill="black"
-                            d="m13.537 12l3.855-3.855a1.091 1.091 0 0 0-1.542-1.541l.001-.001l-3.855 3.855l-3.855-3.855A1.091 1.091 0 0 0 6.6 8.145l-.001-.001l3.855 3.855l-3.855 3.855a1.091 1.091 0 1 0 1.541 1.542l.001-.001l3.855-3.855l3.855 3.855a1.091 1.091 0 1 0 1.542-1.541l-.001-.001z" />
-                    </svg>
-                </div>
+                <v-list selectable v-model:selected="navSelected" :items="items"></v-list>
             </div>
-            <v-list selectable v-model:selected="navSelected" :items="items"></v-list>
         </div>
-    </div>
+    </Transition>
     <div class="surface-0 flex justify-content-center">
         <div id="home" class="w-100 overflow-hidden justify-between">
             <div class="flex w-full px-[1rem] md:px-[12rem] bg-[#0088CC] py-2">
@@ -265,7 +273,28 @@ export default {
     </div>
 </template>
 <style>
-.animatenav {
-    transition: all .5s ease;
+.v-enter-from {
+    height: 100vh;
+    width: 0;
+}
+
+.v-enter-to {
+    width: 75%;
+}
+
+.v-leave-from {
+    width: 75%;
+}
+
+.v-leave-to {
+    width: 0;
+}
+
+.v-leave-active {
+    transition: all 0.5s ease;
+}
+
+.v-enter-active {
+    transition: all 0.5s ease;
 }
 </style>
