39d698da829f9a86274f44afbedffd04e0b2ce45
ichsanputr <ngekode24@gmail.com>
2024-05-29 12:05:06 +0700

    up

---
+++ server/api/potensi-category/index.js
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    let query = '';
+
+    for (let key in getQuery(event)){
+        query += `${key}=${getQuery(event)[key]}&`
+    }
+
+    return await $fetch(useRuntimeConfig().public.API_BASE_URL + '/api/potensi-category?' + query.slice(0, -1))
+})
